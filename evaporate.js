fea4be367aa5b235c6d95027e1d3ae4c42397291
tom <foobarbazbat@gmail.com>
2013-03-29 13:17:35 -0700

    changed copyright date

--- evaporate.js
+++ evaporate.js
@@ -1,4 +1,4 @@
-/*Copyright (c) 2012, TT Labs, Inc.
+/*Copyright (c) 2013, TT Labs, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
