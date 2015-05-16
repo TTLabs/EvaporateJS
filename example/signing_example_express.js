3f3981c100d157921dcc102761084af13142cb50
Fred Cox <mcfedr@gmail.com>
2015-05-16 01:17:14 +0100

    Adds Express JavaScript example
    Resolves #79.

---
+++ example/signing_example_express.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    crypto = require('crypto');
+
+var app = express();
+
+app.get('/signer', function (req, res) {
+    res.send(
+        crypto
+            .createHmac('sha1', 'YOUR_AWS_SECRET_KEY')
+            .update(req.query.to_sign)
+            .digest('base64')
+    );
+});
+
+app.listen(3000);
