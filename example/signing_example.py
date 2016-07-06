4c0cfc253b0646187d7829b49e7e62d2fbd6b566
lampslave <lampslave@gmail.com>
2016-07-06 07:40:38 +0300

    Fix python example: version compatibility, PEP8

--- example/signing_example.py
+++ example/signing_example.py
@@ -1,24 +1,26 @@
 # example for google app engine
 
-import os
 import webapp2
 
 # needed to sign S3 policy
 import base64
-import hmac, sha
+import hashlib
+import hmac
 
 # get YOUR_AWS_SECRET_KEY from somewhere...
 # from foo import YOUR_AWS_SECRET_KEY
 
+
 class SignAuth(webapp2.RequestHandler):
 
-   def get(self):
-      # TODO: Do something to authenticate this request
-      to_sign = str(self.request.get('to_sign'))
-      signature = base64.b64encode(hmac.new('YOUR_AWS_SECRET_KEY', to_sign, sha).digest())
-      self.response.headers['Content-Type'] = "text/HTML"
-      self.response.out.write(signature)
+    def get(self):
+        # TODO: Do something to authenticate this request
+        to_sign = str(self.request.get('to_sign')).encode('utf-8')
+        signature = base64.b64encode(
+            hmac.new(b'YOUR_AWS_SECRET_KEY', to_sign, hashlib.sha1).digest())
+        self.response.headers['Content-Type'] = "text/HTML"
+        self.response.out.write(signature)
 
 app = webapp2.WSGIApplication([
-  ('/sign_auth',SignAuth)
-],debug = True)
+    ('/sign_auth', SignAuth)
+], debug=True)
