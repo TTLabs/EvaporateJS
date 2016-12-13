1c584c438177f7a5a32a90263f88cb3eaf8c17e2
profsmallpine <profsmallpine@gmail.com>
2016-12-13 12:46:22 -0700

    Replacing all single quotes instead of first instance in func s3EncodedObjectName

--- evaporate.js
+++ evaporate.js
@@ -1904,7 +1904,7 @@
     var fileParts = fileName.split('/'),
         encodedParts = [];
     fileParts.forEach(function (p) {
-      encodedParts.push(encodeURIComponent(p).replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/'/, "%27"));
+      encodedParts.push(encodeURIComponent(p).replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/'/g, "%27"));
     });
     return encodedParts.join('/');
   }
