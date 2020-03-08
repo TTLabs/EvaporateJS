682ff605a46efeb3c0158f6bdb93b22195742ada
Matheus Elias R. Moreira <mattelias@live.com>
2020-03-08 19:39:26 +0100

    Add webpack bundling and analyzing

---
+++ codemod/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const plugins = [];
+
+if (process.argv.includes('--analyze')) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = {
+  entry: './output/Evaporate.ts',
+  plugins,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
